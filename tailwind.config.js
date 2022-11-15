27946da784976a05f41e63cdd59557ed4da8cca4
trishika02 <35651199+trishika02@users.noreply.github.com>
2022-11-15 07:34:54 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,18 @@ module.exports = {
     screens: {
       xs: "380px",
       sm: "640px",
-      md: "768px",
+      md: "800px",
       lg: "1024px",
       xl: "1280px",
-      "2xl": "1536px",
+      "2xl": "1440px",
+      "3xl": "1680px",
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        Dosis: ["Dosis", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
